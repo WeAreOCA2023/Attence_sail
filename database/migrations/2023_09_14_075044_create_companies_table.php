ce8a05c7d666057e462c389566590457e9f3a5e2
John Doe <kencoding1031@gmail.com>
2023-09-14 17:00:56 +0900

    beforeSetup

---
+++ database/migrations/2023_09_14_075044_create_companies_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->char('company_id', 10)->primary();
+            $table->string('company_name', 255);
+            $table->char('post_code', 7);
+            $table->string('address', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
