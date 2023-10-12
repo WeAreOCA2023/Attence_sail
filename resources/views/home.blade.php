02cc8ed64c8ef370a9689b3ba61ebf7eff42c81f
GigaOwO <giga@GigaOwO.local>
2023-10-12 09:51:22 +0900

    circle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,21 @@
     <div class="container home d-flex justify-content-around h-100 align-items-center">
         <div class="stopWatch bg-white rounded d-flex align-items-center flex-column">
             <h1 class="bg-black text-white text-center rounded">休憩中</h1>
-            <div class="timeDisplay d-flex justify-content-center align-items-lg-center">
-                <p id="stopwatch">00:00:00</p>
-            </div>
+                <div class="player-timer">
+                    <!-- タイマーの下地になるサークル -->
+                    <svg class="player-timer-circle player-timer-track-circle" width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="110" cy="110" r="108.5" stroke="white" stroke-width="3"/>
+                    </svg>
+                    <!-- タイマーの移動する部分のサークル -->
+                    <svg class="player-timer-circle player-timer-moving-circle" width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="110" cy="110" r="108.5" stroke="#B7B7B7" stroke-width="3"/>
+                    </svg>
+                </div>
+                <h3 id="stopwatch">00:00:00</h3>
             <div class="stopwatchButtons">
-                <button id="startStop" onclick="startStop()">Start</button>
-                <button id="reset" onclick="resetStopwatch()">Reset</button>
+                <button id="startStop" onclick="resetStopwatch()">reset</button>
+                <img src="{{ asset('img/start.svg') }}" alt="" class="player-timer-btn" />
             </div>
-        </div>
-        <div class="player-timer">
-            <!-- タイマーの下地になるサークル -->
-            <svg class="player-timer-circle player-timer-track-circle" width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="110" cy="110" r="108.5" stroke="white" stroke-width="3"/>
-            </svg>
-            <!-- タイマーの移動する部分のサークル -->
-            <svg class="player-timer-circle player-timer-moving-circle" width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="110" cy="110" r="108.5" stroke="#B7B7B7" stroke-width="3"/>
-            </svg>
-            <!-- 再生ボタン -->
-            <img src="{{ asset('img/start.svg') }}" alt="" class="player-timer-btn" />
-            <h3 class='timeDisplay'>0:00</h3>
         </div>
         <div class="infoBox d-flex justify-content-between flex-column">
             <div class="taskList bg-white rounded ">
