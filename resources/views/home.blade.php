46beaa9deb20ee4923073bb98f8576d232ff772c
GigaOwO <giga@GigaOwO.local>
2023-10-13 14:27:53 +0900

    circle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <div class="home">
+    <!-- <svg id="progress" width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path id="progressPath" d="M200 5C225.608 5 250.965 10.0438 274.623 19.8435C298.282 29.6432 319.778 44.0068 337.886 62.1142C355.993 80.2216 370.357 101.718 380.157 125.377C389.956 149.035 395 174.392 395 200C395 225.608 389.956 250.965 380.156 274.623C370.357 298.282 355.993 319.778 337.886 337.886C319.778 355.993 298.282 370.357 274.623 380.157C250.965 389.956 225.608 395 200 395C174.392 395 149.035 389.956 125.377 380.156C101.718 370.357 80.2215 355.993 62.1141 337.886C44.0067 319.778 29.6431 298.282 19.8435 274.623C10.0438 250.965 4.99999 225.608 5 200C5.00001 174.392 10.0439 149.035 19.8435 125.377C29.6432 101.718 44.0068 80.2215 62.1143 62.1141C80.2217 44.0067 101.718 29.6431 125.377 19.8434C149.035 10.0438 174.392 4.99998 200 5L200 5Z" stroke="url(#paint0_linear_227_203)" stroke-width="10"/>
+        <defs>
+            <linearGradient id="paint0_linear_227_203" x1="200" y1="0" x2="200" y2="400" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#71FF6F"/>
+                <stop offset="0.324447" stop-color="#C0FF5A"/>
+                <stop offset="0.671875" stop-color="#FFC076"/>
+                <stop offset="1" stop-color="#FF7D7D"/>
+            </linearGradient>
+        </defs>
+    </svg> -->
     <svg id="progress" width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path id="progressPath" d="M200 5C225.608 5 250.965 10.0438 274.623 19.8435C298.282 29.6432 319.778 44.0068 337.886 62.1142C355.993 80.2216 370.357 101.718 380.157 125.377C389.956 149.035 395 174.392 395 200C395 225.608 389.956 250.965 380.156 274.623C370.357 298.282 355.993 319.778 337.886 337.886C319.778 355.993 298.282 370.357 274.623 380.157C250.965 389.956 225.608 395 200 395C174.392 395 149.035 389.956 125.377 380.156C101.718 370.357 80.2215 355.993 62.1141 337.886C44.0067 319.778 29.6431 298.282 19.8435 274.623C10.0438 250.965 4.99999 225.608 5 200C5.00001 174.392 10.0439 149.035 19.8435 125.377C29.6432 101.718 44.0068 80.2215 62.1143 62.1141C80.2217 44.0067 101.718 29.6431 125.377 19.8434C149.035 10.0438 174.392 4.99998 200 5L200 5Z" stroke="url(#paint0_linear_227_203)" stroke-width="10"/>
         <defs>
@@ -13,90 +24,133 @@
             </linearGradient>
         </defs>
     </svg>
-    <div id="timerDisplay">00:00:00</div>
-    <button id="startButton">Start</button>
-    <button id="stopButton">Stop</button>
-    <button id="resetButton">Reset</button>
+    <div class="button">
+        <p id="timer">00:00:00</p>
+        <ul id="btn">
+        <li class="btn-item" id="start" >スタート</li>
+        <li class="btn-item disabled" id="stop">ストップ</li>
+        <li class="btn-item disabled" id="reset" >リセット</li>
+        </ul>
+    </div>
 </div>
-
 <script>
-    let timerInterval;  // タイマーを管理する setInterval の ID
-    let startTime;      // タイマーが開始された時間
-    let elapsedTime = 0;  // 経過した時間（ミリ秒単位）
-    let isRunning = false;  // タイマーが動作中かどうかの状態
-    const goalTime = 60 * 1000;  // 目標時間（ミリ秒単位）
+    $(document).ready(function() {
+        let setTimeoutId = undefined;
+        let startTime = 0;
+        let currentTime = 0;
+        let elapsedTime = 0;
+        let interval;
+        const totalDuration = 8 * 60 * 60 * 1000;
+        const progressBar = document.getElementById('progressPath');
 
-    // SVGのパス要素
-    const svgPath = document.getElementById('progressPath');
+        
+        function runTimer(){
+            currentTime = Date.now();
+            interval = setInterval(updateProgressBar, 1000);
+            showTime();
+            setTimeoutId = setTimeout(() => {
+            runTimer();
+            },10)
+        }
+        
+        function showTime(){
+            let d = new Date(currentTime - startTime + elapsedTime);
+            const getMin = d.getMinutes();
+            const getSec =d.getSeconds();
+            const getMillisec = Math.floor(d.getMilliseconds() / 10);
+            $("#timer").text(`${String(getMin).padStart(2,'0')}:${String(getSec).padStart(2,'0')}:${String(getMillisec).padStart(2,'0')}`);
+        }
 
-    window.addEventListener('DOMContentLoaded', () => {
-        // ページ読み込み時に円を非表示にする
-        svgPath.style.strokeDasharray = '0';  // 初めは表示しないようにする
-        updateSVG(0);
+        function classReplacementRun()  {
+            $("#start").addClass("disabled");
+            $("#stop").removeClass("disabled");
+            $("#reset").addClass("disabled");
+        }
+
+        function classReplacementStop()  {
+            $("#start").removeClass("disabled");
+            $("#stop").addClass("disabled");
+            $("#reset").removeClass("disabled");
+        }
+
+        function classReplacementInitial()  {
+            $("#start").removeClass("disabled");
+            $("#stop").addClass("disabled");
+            $("#reset").addClass("disabled");
+        }
+
+            // プログレスバーを更新する関数
+        function updateProgressBar() {
+            const progress = Math.min(((currentTime - startTime + elapsedTime) / totalDuration) * 1256, 1256); // 最大100%まで
+            progressBar.style.strokeDashoffset = 1256 - progress;
+            
+            // const hours = Math.floor(currentTime / (60 * 60 * 1000));
+            // const minutes = Math.floor((currentTime % (60 * 60 * 1000)) / (60 * 1000));
+            // const seconds = Math.floor((currentTime % (60 * 1000)) / 1000);
+            // const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            // document.getElementById('timer').textContent = formattedTime;
+        }
+
+
+        $("#start").click(function() {
+            if($(this).hasClass('disabled')){
+            return;
+            }
+            classReplacementRun()
+            startTime = Date.now();
+            runTimer();
+        });
+
+        $("#stop").click(function() {
+            if($(this).hasClass('disabled')){
+            return;
+            }
+            classReplacementStop()
+            elapsedTime += currentTime - startTime;
+            clearTimeout(setTimeoutId);
+        });
+
+        $("#reset").click(function() {
+            if($(this).hasClass('disabled')){
+            return;
+            }
+            classReplacementInitial()
+            clearTimeout(setTimeoutId);
+            elapsedTime = 0
+            $("#timer").text("00:00:00");
+        });
+
     });
 
-    // タイマーを開始する関数
-    function startTimer() {
-        if (!isRunning) {
-            startTime = Date.now() - elapsedTime;
-            timerInterval = setInterval(updateDisplay, 10);
-            isRunning = true;
-            svgPath.style.strokeDasharray = '1000';
-        }
-    }
+    // const totalDuration = 60 * 1000;
+    // let currentTime = 0;
+    // let interval;
+    // const progressBar = document.getElementById('progressPath');
 
-    // タイマーを停止する関数
-    function stopTimer() {
-        if (isRunning) {
-            clearInterval(timerInterval);
-            isRunning = false;
-        }
-    }
+    // // スタートボタンのクリックイベント
+    // document.getElementById('start').addEventListener('click', function() {
+    //     interval = setInterval(updateProgressBar, 1000);
+    //     this.classList.add('disabled');
+    //     document.getElementById('stop').classList.remove('disabled');
+    //     document.getElementById('reset').classList.remove('disabled');
+    // });
 
-    // タイマーをリセットする関数
-    function resetTimer() {
-        stopTimer();
-        elapsedTime = 0;
-        updateDisplay();
-    }
+    // // ストップボタンのクリックイベント
+    // document.getElementById('stop').addEventListener('click', function() {
+    //     clearInterval(interval);
+    //     this.classList.add('disabled');
+    //     document.getElementById('start').classList.remove('disabled');
+    // });
 
-    // タイマーの表示を更新する関数
-    function updateDisplay() {
-        const currentTime = Date.now();
-        elapsedTime = currentTime - startTime;
-        const formattedTime = formatTime(elapsedTime);
-        document.getElementById('timerDisplay').textContent = formattedTime;
+    // // リセットボタンのクリックイベント
+    // document.getElementById('reset').addEventListener('click', function() {
+    //     clearInterval(interval);
+    //     currentTime = 0;
+    //     updateProgressBar();
+    //     document.getElementById('start').classList.remove('disabled');
+    //     this.classList.add('disabled');
+    // });
 
-        updateSVG(elapsedTime);
-    }
-
-    // 時間をフォーマットする関数（HH:mm:ss 形式）
-    function formatTime(milliseconds) {
-        const date = new Date(milliseconds);
-        const hours = date.getUTCHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        const seconds = date.getSeconds().toString().padStart(2, '0');
-        return `${hours}:${minutes}:${seconds}`;
-    }
-
-    // SVGの表示を更新する関数
-    function updateSVG(elapsedTime) {
-        // const progress = Math.min((elapsedTime / goalTime) * 100, 100);
-        const circumference = svgPath.getTotalLength();
-        // const dashoffset = circumference * (progress / 100);
-        const dashoffset = circumference - (elapsedTime / goalTime) * circumference;
-        svgPath.style.strokeDashoffset = dashoffset; 
-        console.log(dashoffset);
-    }
-
-    // "Start" ボタンがクリックされた時の処理
-    document.getElementById('startButton').addEventListener('click', startTimer);
-
-    // "Stop" ボタンがクリックされた時の処理
-    document.getElementById('stopButton').addEventListener('click', stopTimer);
-
-    // "Reset" ボタンがクリックされた時の処理
-    document.getElementById('resetButton').addEventListener('click', resetTimer);
 
 </script>
 @endsection
