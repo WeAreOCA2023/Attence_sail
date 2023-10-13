e934aa73fc591f5ca1315494c50ff86ccb5b53e5
GigaOwO <giga@GigaOwO.local>
2023-10-14 05:16:31 +0900

    header

--- resources/views/layouts/fixed.blade.php
+++ resources/views/layouts/fixed.blade.php
@@ -25,7 +25,7 @@
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="fa-solid fa-gear padding" style="color: #0F1419;"></i>
                 <div class="d-flex flex-flow">
-                    <span class="fs-4 fw-bold">{{ $user_name }}</span>
+                    <span class="fw-bold">{{ $user_name }}</span>
                 </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
