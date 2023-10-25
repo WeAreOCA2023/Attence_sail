92279fff49ebc4a59ba331fb337b3c276dbb82d7
JHSAND <shotaro0106t@gmail.com>
2023-10-25 17:29:12 +0900

    department CREATE - done

--- resources/views/department-management.blade.php
+++ resources/views/department-management.blade.php
@@ -7,25 +7,32 @@
             <img src="{{ asset('img/department.svg') }}" alt="department icon">
             <h2>部署</h2>
         </div>
-            <div class="department">
-              
+        <div class="department d-flex justify-content-around">
+            @foreach ($departments as $department)
+                <h3>{{ $department->department_name }}</h3>
+                @php
+                    $bossName = DB::table('users')->where('user_id', $department->boss_id)->first()->full_name;
+                @endphp
+                <h3>責任者:{{ $bossName }}</h3>
+                <h3>責任者ID:{{ $department->boss_id }}</h3>
                 <div class="edit-delete">
                     <img src="{{ asset('img/edit.svg') }}" alt="editing icon">
                     <img src="{{ asset('img/delete.svg') }}" alt="deleting icon">
                 </div>
-            </div>
+            @endforeach
+        </div>
     </div>
     <div class="addDepartmentsBox">
         <form method="POST" action="">
             @csrf
             <div class="department">
-                <label for="department">{{ __('部署名') }}</label>
-                <input id="department" type="text" name="email" value="{{ old('department') }}" required autocomplete="department">
+                <label for="departmentName">{{ __('部署名') }}</label>
+                <input id="departmentName" type="text" name="departmentName" value="{{ old('departmentName') }}" required autocomplete="departmentName" autofocus>
             </div>
 
             <div class="responsible">
-                <label for="responsible">{{ __('責任者名') }}</label>
-                <input id="responsible" type="responsible" name="responsible" value="{{ old('responsible') }}" required autocomplete="responsible"> 
+                <label for="bossName">{{ __('責任者名') }}</label>
+                <input id="bossName" type="text" name="bossName" value="{{ old('bossName') }}" required autocomplete="bossName">
             </div>
 
             <div class="createButton">
@@ -33,7 +40,7 @@
                     {{ __('作成') }}
                 </button>
             </div>
-            
+
         </form>
     </div>
 </div>
