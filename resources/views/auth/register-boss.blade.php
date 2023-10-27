392d9d3f2fc93f38fbfca7dd270f8028f7b0a0b4
GigaOwO <takumagiga1118@gmail.com>
2023-10-27 18:43:51 +0900

    bossregister

--- resources/views/auth/register-boss.blade.php
+++ resources/views/auth/register-boss.blade.php
@@ -1,160 +1,135 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 @section('content')
-<div class="container h-100 d-flex justify-content-center align-items-center register">
-    <div class="row justify-content-center">
-        <div class="col-md-4 px-0">
-            <div class="card h-100 reset-rounded">
-                <div class="flex card-body">
-                    <h2 class="text-center mb-1 h1 mainColor">サインアップ（ボス）</h2>
-                    <form method="POST" action="{{ route('register-boss') }}">
-                        @csrf
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="companyName" class="col-form-label">{{ __('会社名') }}</label>
-
-                            <div class="col">
-                                <input id="companyName" type="text" class="form-control @error('companyName') is-invalid @enderror" name="companyName" value="{{ old('companyName') }}" required autocomplete="companyName" autofocus>
-
-                                @error('companyName')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<div class="registerBoss">
+    <div class="main d-flex flex-column align-items-center justify-content-between">
+        <div class="logo">
+            <img src="{{ asset('img/logo-white.svg') }}" alt="logo">
+        </div>
+        <div class="loginBox d-flex flex-column align-items-center justify-content-center">
+            <div class="loginBoxInner d-flex flex-column justify-content-center align-items-center">
+                <h1>サインアップ（ボス）</h1>
+                <form class="d-flex flex-column align-items-center justify-content-between" method="POST" action="{{ route('register-boss') }}">
+                    @csrf
+                    <div class="companyNameBox">
+                        <label for="companyName" class="col-form-label">{{ __('会社名') }}</label>
+                        <div class="companyNameInput">
+                            <input id="companyName" type="text" class="form-control @error('companyName') is-invalid @enderror" name="companyName" value="{{ old('companyName') }}" required autocomplete="companyName" autofocus>
+                            @error('companyName')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="companyCode" class="col-form-label">{{ __('会社コード') }}</label>
-                            <div class="col">
-                                <input id="companyCode" type="text" class="form-control @error('companyCode') is-invalid @enderror" name="companyCode" minlength=12 maxlength=12 value="{{ old('companyCode') }}" required autocomplete="companyCode" >
-                            </div>
+                    </div>
+                    <div class="companyCodeBox">
+                        <label for="companyCode" class="col-form-label">{{ __('会社コード') }}</label>
+                        <div class="companyCodeInput">
+                            <input id="companyCode" type="text" class="form-control @error('companyCode') is-invalid @enderror" name="companyCode" minlength=12 maxlength=12 value="{{ old('companyCode') }}" required autocomplete="companyCode" >
                         </div>
-                        <div class="row mb-4 form-input w-50 mx-auto mainColor">
-                            <label for="companyPassword" class="col-form-label">{{ __('会社パスワード') }}</label>
-                            <div class="col">
-                                <input id="companyPassword" type="password" class="form-control @error('companyPassword') is-invalid @enderror" name="companyPassword" value="{{ old('companyPassword') }}" required autocomplete="companyPassword">
-                                @error('companyPassword')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="companyPasswordBox">
+                        <label for="companyPassword" class="col-form-label">{{ __('会社パスワード') }}</label>
+                        <div class="companyPasswordInput">
+                            <input id="companyPassword" type="password" class="form-control @error('companyPassword') is-invalid @enderror" name="companyPassword" value="{{ old('companyPassword') }}" required autocomplete="companyPassword">
+                            @error('companyPassword')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="companyPostCode" class="col-form-label">{{ __('会社の郵便番号') }}</label>
-
-                            <div class="col">
-                                <input id="companyPostCode" type="text" class="form-control @error('companyPostCode') is-invalid @enderror" name="companyPostCode" minlength=7 maxlength=7 value="{{ old('companyPostCode') }}" required autocomplete="companyPostCode">
-
-                                @error('companyAddress')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="companyPostCodeBox">
+                        <label for="companyPostCode" class="col-form-label">{{ __('会社の郵便番号') }}</label>
+                        <div class="companyPostCodeInput">
+                            <input id="companyPostCode" type="text" class="form-control @error('companyPostCode') is-invalid @enderror" name="companyPostCode" minlength=7 maxlength=7 value="{{ old('companyPostCode') }}" required autocomplete="companyPostCode">
+                            @error('companyAddress')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="companyAddress" class="col-form-label">{{ __('会社の住所') }}</label>
-
-                            <div class="col">
-                                <input id="companyAddress" type="text" class="form-control @error('companyAddress') is-invalid @enderror" name="companyAddress" value="{{ old('companyAddress') }}" required autocomplete="companyAddress">
-
-                                @error('companyAddress')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="companyAddressBox">
+                        <label for="companyAddress" class="col-form-label">{{ __('会社の住所') }}</label>
+                        <div class="companyAddressInput">
+                            <input id="companyAddress" type="text" class="form-control @error('companyAddress') is-invalid @enderror" name="companyAddress" value="{{ old('companyAddress') }}" required autocomplete="companyAddress">
+                            @error('companyAddress')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="userName" class="col-form-label">{{ __('ユーザー名') }}</label>
-
-                            <div class="col">
-                                <input id="userName" type="text" class="form-control @error('userName') is-invalid @enderror" name="userName" value="{{ old('userName') }}" required autocomplete="userName">
-
-                                @error('userName')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="userNameBox">
+                        <label for="userName" class="col-form-label">{{ __('ユーザー名') }}</label>
+                        <div class="userNameInput">
+                            <input id="userName" type="text" class="form-control @error('userName') is-invalid @enderror" name="userName" value="{{ old('userName') }}" required autocomplete="userName">
+                            @error('userName')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="fullName" class="col-form-label">{{ __('名前') }}</label>
-
-                            <div class="col">
-                                <input id="fullName" type="text" class="form-control @error('fullName') is-invalid @enderror" name="fullName" value="{{ old('fullName') }}" required autocomplete="fullName">
-
-                                @error('fullName')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="fullNameBox">
+                        <label for="fullName" class="col-form-label">{{ __('名前') }}</label>
+                        <div class="fullNameInput">
+                            <input id="fullName" type="text" class="form-control @error('fullName') is-invalid @enderror" name="fullName" value="{{ old('fullName') }}" required autocomplete="fullName">
+                            @error('fullName')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="email" class="col-form-label">{{ __('メールアドレス') }}</label>
-
-                            <div class="col">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="emailBox">
+                        <label for="email" class="col-form-label">{{ __('メールアドレス') }}</label>
+                        <div class="emailInput">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="telephone" class="col-form-label">{{ __('電話番号') }}</label>
-                            <div class="col">
-                                <input id="telephone" type="tel" class="form-control @error('telephone') is-invalid @enderror" minlength=11 maxlength=11 name="telephone" required autocomplete="telephone">
-
-                                @error('telephone')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="telephoneBox">
+                        <label for="telephone" class="col-form-label">{{ __('電話番号') }}</label>
+                        <div class="telephoneInput">
+                            <input id="telephone" type="tel" class="form-control @error('telephone') is-invalid @enderror" minlength=11 maxlength=11 name="telephone" required autocomplete="telephone">
+                            @error('telephone')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="password" class="col-form-label">{{ __('パスワード') }}</label>
-
-                            <div class="col">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                    <div class="passwordBox">
+                        <label for="password" class="col-form-label">{{ __('パスワード') }}</label>
+                        <div class="passwordInput">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
-                        <div class="row mb-1 form-input w-50 mx-auto mainColor">
-                            <label for="password-confirm" class="col-form-label">{{ __('パスワードの確認') }}</label>
-
-                            <div class="col">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                    </div>
+                    <div class="passwordConfirmBox">
+                        <label for="password-confirm" class="col-form-label">{{ __('パスワードの確認') }}</label>
+                        <div class="passwordConfirmInput">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
-                        <input type="hidden" name="is_boss" value="1">
-                        <div class="row mb-0">
-                            <div class="login-form col-md-8 offset-md-4 text-center mx-auto d-flex align-items-start">
-                                <button type="submit" class="text-decoration-none mx-auto">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <input type="hidden" name="is_boss" value="1">
+                    <div class="RegisterButton">
+                        <button type="submit">
+                            {{ __('Register') }}
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="col-md-4 title px-0 d-flex align-items-center">
-            <img src="{{ asset('img/tree.png') }}" class="img-fluid rounded-end float-start" alt="木">
-            <div class="text-center mb-1 logo">
-                <img src="{{ asset('img/logo-gray-150.png') }}" alt="logo">
-            </div>
-            <h1>ATTENCE</h1>
-        </div>
-
     </div>
 </div>
 @endsection
