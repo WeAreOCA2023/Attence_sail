cebd8c54997e5a870467955bf5fea7bd1617fc76
GigaOwO <takumagiga1118@gmail.com>
2023-10-31 16:09:24 +0900

    task

--- resources/views/my-all-tasks.blade.php
+++ resources/views/my-all-tasks.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.fixed')
 
 @section('content')
-<div class="myAllTasks d-flex w-100 justify-content-center align-items-center">
-    <div class="jobList">
+    <div class="myAllTasks d-flex w-100 justify-content-center align-items-center">
+        <div class="jobList">
+            <div class="title">
+                <img src="{{ asset('img/Onprogress.svg') }}" alt="">
+            </div>
+        </div>
+        <div class="job">
 
+        </div>
     </div>
-    <div class="job">
-
-    </div>
-</div>
 @endsection
