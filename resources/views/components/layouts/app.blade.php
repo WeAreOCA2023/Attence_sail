6604ef0c8bbb705f02cb47b79baa58b4aa46962f
GigaOwO <takumagiga1118@gmail.com>
2023-11-03 02:24:15 +0900

    tasks

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,9 +2,18 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('index.name', 'Laravel') }}</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN">
+
+        <!-- Scripts -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <script src="https://kit.fontawesome.com/48efdb6da3.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     </head>
     <body>
         {{ $slot }}
---
+++ resources/views/task.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.fixed')
+
+@section('content')
+<div class="myAllTasks d-flex w-100 justify-content-center align-items-center">
+    <div class="jobList position-relative">
+        <div class="title d-flex">
+            <img src="{{ asset('img/Onprogress.svg') }}" alt="">
+            <h2>未完了</h2>
+        </div>
+        <div class="position-absolute plusIcon">
+            <img src="{{ asset('img/plus.svg') }}" alt="">
+        </div>
+        <div>
+            <h1>{{ $count }}</h1>
+
+            <button wire:click="increment">+</button>
+
+            <button wire:click="decrement">-</button>
+        </div>
+    </div>
+    <div class="job">
+
+    </div>
+</div>
+@endsection
