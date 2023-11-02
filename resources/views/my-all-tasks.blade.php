bee94bf5193272d7891229e947e9b84d1e95eb9b
GigaOwO <takumagiga1118@gmail.com>
2023-11-02 18:22:22 +0900

    livewire

--- resources/views/my-all-tasks.blade.php
+++ resources/views/my-all-tasks.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
     <div class="myAllTasks d-flex w-100 justify-content-center align-items-center">
-        <div class="jobList">
+        <div class="jobList position-relative">
             <div class="title d-flex">
                 <img src="{{ asset('img/Onprogress.svg') }}" alt="">
                 <h2>未完了</h2>
+            </div>
+            <div class="position-absolute plusIcon">
+                <img src="{{ asset('img/plus.svg') }}" alt="">
             </div>
         </div>
         <div class="job">
