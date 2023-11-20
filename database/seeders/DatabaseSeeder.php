2e3e9648b5cd0bf36773c7411b95f9acbca6af50
JHSAND <shotaro0106t@gmail.com>
2023-11-20 09:44:42 +0900

    dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Position;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -22,8 +22,27 @@ class DatabaseSeeder extends Seeder
         // \App\Models\UserLogin::factory(20)->create();
         // \App\Models\Position::factory(20)->create();
         // \App\Models\Department::factory(20)->create();
-        \App\Models\User::factory(20)->create();
-
-
+        // \App\Models\User::factory(20)->create();
+        $positions = [
+            '代表取締役社長' => 100,
+            '副社長' => 90,
+            '専務取締役' => 92,
+            '常務取締役' => 85,
+            '本部長' => 80,
+            '部長' => 75,
+            '次長' => 70,
+            '課長' => 65,
+            '係長' => 60,
+            '主任' => 55,
+            '一般' => 5,
+            '派遣社員(A株式会社)' => 1,
+            '業務委託先(B株式会社)' => 1
+        ];
+        foreach ($positions as $position_name => $rank) {
+            Position::factory()->create([
+                'position_name' => $position_name,
+                'rank' => $rank,
+            ]);
+        }
     }
 }
