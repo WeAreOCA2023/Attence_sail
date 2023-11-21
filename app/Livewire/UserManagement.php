b6d97209192756c59223e016124f4938dd8ba011
JHSAND <shotaro0106t@gmail.com>
2023-11-21 22:39:56 +0900

    department filter done

--- app/Livewire/UserManagement.php
+++ app/Livewire/UserManagement.php
@@ -27,8 +27,9 @@ class UserManagement extends Component
     public $assignPositionId;
 
     // フィルター用
-    public $fileterDepartmentId = null;
+    public $filterDepartmentId = null;
     public $filterPositionId = null;
+
 
     public function render()
     {
@@ -36,7 +37,22 @@ class UserManagement extends Component
         $assignable_positions = Position::select('id', 'position_name')->get();
         $users_info = [];
         $company_id = User::where('user_id', Auth::user()->id)->first()->company_id;
-        $users_table_pagination = User::where('company_id', $company_id)->search('full_name', $this->search_user)->orderBy('user_id', 'asc')->paginate(12);
+        if ($this->filterDepartmentId != null) {
+            if (strlen($this->search_user >= 0)) {
+                $users_table_pagination = User::where('company_id', $company_id)
+                ->where('department_id', $this->filterDepartmentId)
+                ->search('full_name', $this->search_user)
+                ->orderBy('user_id', 'asc')
+                ->paginate(12);
+            } else{
+                $users_table_pagination = User::where('company_id', $company_id)
+                ->where('department_id', $this->filterDepartmentId)
+                ->orderBy('user_id', 'asc')
+                ->paginate(12);
+            }
+        } else {
+            $users_table_pagination = User::where('company_id', $company_id)->search('full_name', $this->search_user)->orderBy('user_id', 'asc')->paginate(12); 
+        }
         foreach ($users_table_pagination as $user_pagination) {
             $user = User::where('user_id', $user_pagination->user_id)->first();
             $user_login = UserLogin::where('id', $user->user_id)->first();
@@ -105,6 +121,9 @@ class UserManagement extends Component
                 'assignable_positions' => $assignable_positions,
             ];      
         }
+        // if ($this->filterDepartmentId != null) {
+        //     dd($users_info);
+        // }
         $all_departments = Department::select('id', 'department_name')->get();
         $all_positions = Position::select('id', 'position_name')->get();
         return view('livewire.user-management', [
@@ -135,103 +154,13 @@ class UserManagement extends Component
         return redirect('/user-management');
     }
 
-
     public function filterDepartment($id)
     {
-        $this->fileterDepartmentId = $id;
-        $this->filterPositionId = null;
-        $assinable_departments = Department::select('id', 'department_name')->get();
-        $assignable_positions = Position::select('id', 'position_name')->get();
-        $users_info = [];
-        // You can reuse most of the existing code with a slight modification in the query
-        $company_id = User::where('user_id', Auth::user()->id)->first()->company_id;
-    
-        // Add the department filter to the query
-        $users_table_pagination = User::where('company_id', $company_id)
-            ->search('full_name', $this->search_user)
-            ->where('department_id', $this->fileterDepartmentId)->orderBy('user_id', 'asc')->paginate(12);
-        $this->resetPage();
-        // Rest of your existing code to populate $users_info
-        foreach ($users_table_pagination as $user_pagination) {
-            $user = User::where('user_id', $user_pagination->user_id)->first();
-            $user_login = UserLogin::where('id', $user->user_id)->first();
-            $department_id = $user->department_id;
-            $user = User::where('user_id', $user_pagination->user_id)->first();
-            $user_login = UserLogin::where('id', $user->user_id)->first();
-            $department_id = $user->department_id;
-            if ($department_id == 0) {
-                $department_name = '<span class="unset">' . '未設定' . '</span>';
-            } elseif($department_id == -1){
-                $department_name = '<span class="unaffliated">' . '無し' . '</span>';
-            } else {
-                $department_name = Department::where('id', $department_id)->first()->department_name;
-            }
-            $position_id = $user->position_id;
-            if ($position_id == 0) {
-                $position_name = '<span class="unset">' . '未設定' . '</span>';
-            } else {
-                $position_name = Position::where('id', $position_id)->first()->position_name;
-            }
-            $agreement_36 = $user->agreement_36;
-            if ($agreement_36 == 0) {
-                $agreement_36 = '<span class="unset">' . '未設定' . '</span>';
-            } elseif ($agreement_36 == 1) {
-                $agreement_36 = '有り';
-            } elseif ($agreement_36 == 2) {
-                $agreement_36 = '特別条項付き';
-            } else {
-                $agreement_36 = '無し';
-            }
-            $variable_working_hours_system = $user->variable_working_hours_system;
-            if ($variable_working_hours_system == 0) {
-                $variable_working_hours_system = '<span class="unset">' . '未設定' . '</span>';
-            } elseif ($variable_working_hours_system == 1) {
-                $variable_working_hours_system = '有り';
-            } else {
-                $variable_working_hours_system = '無し';
-            }
-            $status = $user->status;
-            if ($status == 0) {
-                $status = '<span class="status-offline">' . '未出勤' . '</span>';
-            } elseif ($status == 1) {
-                $status = '<span class="status-online">' . '出勤中' . '</span>';
-            } elseif ($status == 2) {
-                $status = '<span class="status-break">' . '休憩中' . '</span>';
-            } elseif ($status == 3) {
-                $status = '<span class="status-leave">' . '休職中' . '</span>';
-            } else {
-                $status = '<span class="status-offboarding">' . '退職済' . '</span>';
-            }
-            $over_work = $user->over_work;
-            if ($over_work == 0) {
-                $over_work = '<span class="status-unoverwork">' . '正常' . '</span>';
-            } else {
-                $over_work = '<span class="status-overwork">' . '警告' . '</span>';
-            }
-            // user-managementで使用するデータ
-            $users_info[$user->user_id] = [
-                'user_id' => $user->user_id,
-                'full_name' => $user->full_name,
-                'email' => $user_login->email,
-                'department_name' => $department_name,
-                'position_name' => $position_name,
-                'agreement_36' => $agreement_36,
-                'variable_working_hours_system' => $variable_working_hours_system,
-                'status' => $status,
-                'over_work' => $over_work,
-                'assignable_departments' => $assinable_departments,
-                'assignable_positions' => $assignable_positions,
-            ];  
-        }   
-        $all_departments = Department::select('id', 'department_name')->get();
-        $all_positions = Position::select('id', 'position_name')->get();
-        // Pass the filtered users to the view
-        return view('livewire.user-management', [
-            'search_users' => $users_table_pagination,
-            'users_info' => $users_info,
-            'all_departments' => $all_departments,
-            'all_positions' => $all_positions,
-        ]);
+        if ($id == 0) {
+            $this->filterDepartmentId = null;
+        }
+        $this->filterDepartmentId = $id;
     }
+
 
 }
--- resources/views/livewire/user-management.blade.php
+++ resources/views/livewire/user-management.blade.php
@@ -12,6 +12,7 @@
                     部署
                 </a>
                 <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" wire:click="filterDepartment(0)">取り消す</a></li>
                     @foreach ($all_departments as $department)
                         <li><a class="dropdown-item" wire:click="filterDepartment({{ $department->id }})">{{ $department->department_name }}</a></li>
                     @endforeach
@@ -74,140 +75,114 @@
                 <strong>{{ session('successUser') }}</strong>
             </div>
         @endif
-        @if (isset($filterDepartmentId))
-            <table class="table table-hover">
-                <thead class="table-dark">
+        <table class="table table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th></th>
+                    <th>名前</th>
+                    <th>メールアドレス</th>
+                    <th>部署</th>
+                    <th>役職</th>
+                    <th class="th-agreement36">36協定</th>
+                    <th class="th-variable_working_hours_system">変形労働時間制</th>
+                    <th class="th-status">ステータス</th>
+                    <th class="th-overwork">超過労働</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            @if (strlen($search_users) > 0)
+                @foreach ($users_info as $user_info)
+                    @if ($editing == true && $editUserId === $user_info['user_id'])
                     <tr>
-                        <th></th>
-                        <th>名前</th>
-                        <th>メールアドレス</th>
-                        <th>部署</th>
-                        <th>役職</th>
-                        <th class="th-agreement36">36協定</th>
-                        <th class="th-variable_working_hours_system">変形労働時間制</th>
-                        <th class="th-status">ステータス</th>
-                        <th class="th-overwork">超過労働</th>
-                        <th></th>
+                        <th scope="row">{{ $user_info['user_id'] }}</th>
+                        <td>{{ $user_info['full_name'] }}</td>
+                        <td>{{ $user_info['email'] }}</td>
+                        <td class="td-department">
+                            <select wire:click="filterDepartment({{ $user_info['department_id'] }})" class="form-select" aria-label="assignable departments">
+                                <option selected>選択してください</option>
+                                <option value="1">無し</option>
+                                @foreach ($user_info['assignable_departments'] as $assignable_department)
+                                    <option value="{{ $assignable_department->id }}">{{ $assignable_department->department_name }}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                        <td>
+                            <select wire:model="assignPositionId" class="form-select" aria-label="asignable positions">
+                                <option selected>選択してください</option>
+                                @foreach ($user_info['assignable_positions'] as $assignable_position)
+                                    <option value="{{ $assignable_position->id }}">{{ $assignable_position->position_name }}</option>
+                                @endforeach
+                        </td>
+                        <td>{!! $user_info['agreement_36'] !!}</td>
+                        <td>{!! $user_info['variable_working_hours_system'] !!}</td>
+                        <td>{!! $user_info['status'] !!}</td>
+                        <td>{!! $user_info['over_work'] !!}</td>
+                        <td>
+                            <div class="save-cancel d-flex justify-content-between align-items-center">
+                                <button wire:click="update({{ $user_info['user_id'] }})">
+                                    <img class="saveImg" src="{{ asset('img/save.svg') }}" alt="saving icon">
+                                </button>
+                                <button wire:click="$set('editing', false)" class="cancelBtn btn-primary">
+                                    <img class="cancelImg" src="{{ asset('img/cancel.svg') }}" alt="canceling icon">
+                                </button>
+                            </div>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                @if (strlen($search_users) > 0)
-                    @foreach ($users_info as $user_info)
-                        @if ($editing == true && $editUserId === $user_info['user_id'])
-                        <tr>
-                            <th scope="row">{{ $user_info['user_id'] }}</th>
-                            <td>{{ $user_info['full_name'] }}</td>
-                            <td>{{ $user_info['email'] }}</td>
-                            <td class="td-department">
-                                <select wire:model="assignDepartmentId" class="form-select" aria-label="assignable departments">
-                                    <option selected>選択してください</option>
-                                    <option value="1">無し</option>
-                                    @foreach ($user_info['assignable_departments'] as $assignable_department)
-                                        <option value="{{ $assignable_department->id }}">{{ $assignable_department->department_name }}</option>
-                                    @endforeach
-                                </select>
-                            </td>
-                            <td>
-                                <select wire:model="assignPositionId" class="form-select" aria-label="asignable positions">
-                                    <option selected>選択してください</option>
-                                    @foreach ($user_info['assignable_positions'] as $assignable_position)
-                                        <option value="{{ $assignable_position->id }}">{{ $assignable_position->position_name }}</option>
-                                    @endforeach
-                            </td>
-                            <td>{!! $user_info['agreement_36'] !!}</td>
-                            <td>{!! $user_info['variable_working_hours_system'] !!}</td>
-                            <td>{!! $user_info['status'] !!}</td>
-                            <td>{!! $user_info['over_work'] !!}</td>
-                            <td>
-                                <div class="save-cancel d-flex justify-content-between align-items-center">
-                                    <button wire:click="update({{ $user_info['user_id'] }})">
-                                        <img class="saveImg" src="{{ asset('img/save.svg') }}" alt="saving icon">
-                                    </button>
-                                    <button wire:click="$set('editing', false)" class="cancelBtn btn-primary">
-                                        <img class="cancelImg" src="{{ asset('img/cancel.svg') }}" alt="canceling icon">
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        @else
-                        <tr>
-                            <th scope="row">{{ $user_info['user_id'] }}</th>
-                            <td class="td-fullName">{{ $user_info['full_name'] }}</td>
-                            <td class="td-email">{{ $user_info['email'] }}</td>
-                            <td class="td-department">{!! $user_info['department_name'] !!}</td>
-                            <td  class="td-position">{!! $user_info['position_name'] !!}</td>
-                            <td>{!! $user_info['agreement_36'] !!}</td>
-                            <td>{!! $user_info['variable_working_hours_system'] !!}</td>
-                            <td>{!! $user_info['status'] !!}</td>
-                            <td>{!! $user_info['over_work'] !!}</td>
-                            <td class="td-Btn">
-                                <div class="edit-delete d-flex justify-content-between align-items-center">
-                                    <button wire:click="edit({{ $user_info['user_id'] }})">
-                                        <img src="{{ asset('img/edit.svg') }}" alt="editing icon">
-                                    </button>                                
-                                    <button class="deleteBtn btn-primary" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal{{ $user_info['user_id'] }}">
-                                        <img src="{{ asset('img/delete.svg') }}" alt="deleting icon">
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <div class="modal fade" id="confirmDeleteModal{{ $user_info['user_id'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">本当に削除しますか？</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>メールアドレス: {{ $user_info['email'] }}</p>
-                                                <p>名前: {{ $user_info['full_name'] }}</p>
-                                                <p>を削除すると、元には戻せません</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
-                                                <form class="delete" method="POST" action="{{ route('user-management.destroy',$user_info['user_id']) }}">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="btn btn-primary">削除</button>
-                                                </form>
-                                            </div>
+                    @else
+                    <tr>
+                        <th scope="row">{{ $user_info['user_id'] }}</th>
+                        <td class="td-fullName">{{ $user_info['full_name'] }}</td>
+                        <td class="td-email">{{ $user_info['email'] }}</td>
+                        <td class="td-department">{!! $user_info['department_name'] !!}</td>
+                        <td  class="td-position">{!! $user_info['position_name'] !!}</td>
+                        <td>{!! $user_info['agreement_36'] !!}</td>
+                        <td>{!! $user_info['variable_working_hours_system'] !!}</td>
+                        <td>{!! $user_info['status'] !!}</td>
+                        <td>{!! $user_info['over_work'] !!}</td>
+                        <td class="td-Btn">
+                            <div class="edit-delete d-flex justify-content-between align-items-center">
+                                <button wire:click="edit({{ $user_info['user_id'] }})">
+                                    <img src="{{ asset('img/edit.svg') }}" alt="editing icon">
+                                </button>                                
+                                <button class="deleteBtn btn-primary" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal{{ $user_info['user_id'] }}">
+                                    <img src="{{ asset('img/delete.svg') }}" alt="deleting icon">
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <div class="modal fade" id="confirmDeleteModal{{ $user_info['user_id'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">本当に削除しますか？</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>メールアドレス: {{ $user_info['email'] }}</p>
+                                            <p>名前: {{ $user_info['full_name'] }}</p>
+                                            <p>を削除すると、元には戻せません</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+                                            <form class="delete" method="POST" action="{{ route('user-management.destroy',$user_info['user_id']) }}">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-primary">削除</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endif
+                        </div>
+                    @endif
 
-                        
-                    @endforeach
-                @endif
-                </tbody>
-            </table>
-        @else
-            <table>
-            <thead class="table-dark">
-                    <tr>
-                        <th></th>
-                        <th>名前</th>
-                        <th>メールアドレス</th>
-                        <th>部署</th>
-                        <th>役職</th>
-                        <th class="th-agreement36">36協定</th>
-                        <th class="th-variable_working_hours_system">変形労働時間制</th>
-                        <th class="th-status">ステータス</th>
-                        <th class="th-overwork">超過労働</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        @foreach ($all_departments as $department)
-                        <td>{{ $department->department_name }}</td>
-                        @endforeach
-                    </tr>
-            </table>
-        @endif
-
+                    
+                @endforeach
+            @endif
+            </tbody>
+        </table>
         <div class="pagination-link d-flex justify-content-center align-items-center">
             {{ $search_users->links() }}
         </div>
