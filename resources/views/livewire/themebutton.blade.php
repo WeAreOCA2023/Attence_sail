a8caa8129637e678e598ac3538146e13d6a5791e
GigaOwO <takumagiga1118@gmail.com>
2023-11-21 14:52:47 +0900

    dark mode

---
+++ resources/views/livewire/themebutton.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <li class="form-check form-switch">
+        <input wire:model.live="mode" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+        <label class="form-check-label" for="flexSwitchCheckChecked">{{ $type }}</label>
+    </li>
+</div>
