4b89492791f46df0cfb0c4c4649b190bc394c18d
John Doe <kencoding1031@gmail.com>
2023-11-30 10:26:47 +0900

    componentSetup

--- app/Livewire/Next7Days.php
+++ app/Livewire/Next7Days.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Blade;
 use Livewire\Component;
 
 class Next7Days extends Component
 {
     public function render()
     {
+        Blade::component('each-day', EachDay::class);
         return view('livewire.next-7-days');
     }
 }
--- resources/views/livewire/next-7-days.blade.php
+++ resources/views/livewire/next-7-days.blade.php
@@ -1,3 +1,4 @@
 <div>
     <h1>Next7Days</h1>
+    <x-next-7-each-day></x-next-7-each-day>
 </div>
